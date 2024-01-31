9e1cd11382647720eddbf2eca0d21b2817746149
Ashwinia <ashwinia1233@hotmail.com>
2024-01-31 11:38:35 -0500

    updates

--- Labo 4 - Intentions et permissions/app/src/main/java/com/example/labo4/Activity2.java
+++ Labo 4 - Intentions et permissions/app/src/main/java/com/example/labo4/Activity2.java
@@ -3,12 +3,49 @@ package com.example.labo4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity implements View.OnClickListener {
+
+    // Déclarations
+    EditText editTextPhoneNumber;
+    Button appeler;
+    Button revenirArriere1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        appeler = findViewById(R.id.btnAppeler_id);
+        appeler.setOnClickListener(this);
+
+        revenirArriere1 = findViewById(R.id.btnRevenir1_id);
+        revenirArriere1.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        editTextPhoneNumber = findViewById(R.id.editTxtPhoneNum_id);
+        String strNumeroTelephone = editTextPhoneNumber.getText().toString();
+        if(!strNumeroTelephone.isBlank()){
+            // Concaténation avec tel
+            String completNumero =
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
